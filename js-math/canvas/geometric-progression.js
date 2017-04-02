fd01b274311127b375d3a37b391269af6eb27a22
Rafael Specht <rafaelspdasilva@gmail.com>
2017-04-02 19:30:41 -0300

    improving geometric progression example

--- js-math/canvas/geometric-progression.js
+++ js-math/canvas/geometric-progression.js
@@ -3,12 +3,14 @@ var ctx = canvas.getContext('2d')
 const { width, height } = ctx.canvas
 
 let counter = 0
-const squareSide = 50 * Math.sqrt(2)/2
+
 const initial = 10
 
 for (let i = 0; i < 20; i += 1) {
-  const side = initial * Math.pow(1.41, i)
-  plotSquare(width/2, height/2, side, Math.round(Math.sin(counter % 2)) * 45)
+  const side = initial * Math.pow(Math.sqrt(2), i)
+  const angle = Math.round(Math.sin(counter % 2)) * 45
+
+  plotSquare(width/2, height/2, side, angle)
 
   counter++
 }

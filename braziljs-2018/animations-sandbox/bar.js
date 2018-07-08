ff6654424138d3cf0866f35733daf39949c311e8
Rafael Specht <rafaelspdasilva@gmail.com>
2018-07-08 12:41:27 -0300

    Adding gradient to bars

--- braziljs-2018/animations-sandbox/bar.js
+++ braziljs-2018/animations-sandbox/bar.js
@@ -5,8 +5,13 @@ const { width, height } = ctx.canvas
 
 const barsSample = BARS // eslint-disable-line
 
+// ff1e00 rgb(255, 30, 0)
+// 2c2626 rgb(44,38,38)
+// 5273ff rgb(82,115,255)
+
 let currentRow = 0
-const BAR_WIDTH = 5
+let BAR_WIDTH = 1
+let BAR_SPACE = 1
 
 const draw = () => {
   ctx.clearRect(0, 0, width, height)
@@ -17,20 +22,43 @@ const draw = () => {
     currentRow = 0
   }
 
-  // const color = `hsla(${0}, ${50}%, ${50}%, ${1})`
-
   const bars = barsSample[currentRow]
+
+  BAR_WIDTH = Math.ceil(width / bars.length)
+
+  const red = `rgba(${255}, ${30}, ${0}, ${1})`
+  const blue = `rgba(${82}, ${115}, ${255}, ${1})`
+  // const black = `rgba(${44}, ${38}, ${38}, ${1})`
 
   bars.forEach((value, i) => {
     const colHeight = value * height / 255
-    const color = `rgba(${255}, ${Math.sin(value / 30) * 30 + 30}, ${0}, ${1})`
 
-    ctx.fillStyle = color
+    const x = i * (BAR_SPACE + BAR_WIDTH)
+    const y = height - colHeight
 
-    ctx.fillRect(i * (2 + BAR_WIDTH), height - colHeight, BAR_WIDTH, colHeight)
+    const gradient = ctx.createLinearGradient(x, height, x, value)
+    gradient.addColorStop(0, red)
+    gradient.addColorStop(0.7, blue)
+
+    ctx.fillStyle = gradient
+
+    ctx.fillRect(x, y, BAR_WIDTH, colHeight)
   })
 
   setTimeout(draw, 100)
 }
 
 draw()
+
+// const gradient1 = ctx.createLinearGradient(0, 0, 150, 0)
+// gradient1.addColorStop(0, 'red')
+// gradient1.addColorStop(1, 'blue')
+// ctx.fillStyle = gradient1
+// ctx.fillRect(0, 0, 300, 100)
+//
+// const gradient2 = ctx.createLinearGradient(0, 0, 150, 0)
+// gradient2.addColorStop(0, 'green')
+// gradient2.addColorStop(1, 'white')
+// ctx.fillStyle = gradient2
+//
+// ctx.fillRect(0, 200, 300, 100)

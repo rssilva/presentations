821284352558a2b718106f6f73f49f3ef1685646
Rafael Specht <rafaelspdasilva@gmail.com>
2018-08-05 20:10:07 -0300

    Creating a default skin to the analyser, now we can create skins :dance:

--- braziljs-2018/utils/analyser.js
+++ braziljs-2018/utils/analyser.js
@@ -1,5 +1,61 @@
+class AnalyserDefaultSkin {
+  set ({ fBufferLength, tBufferLength, canvasCtx, barWidth = 3 }) {
+    this.fBufferLength = fBufferLength
+    this.tBufferLength = tBufferLength
+    this.canvasCtx = canvasCtx
+    this.barWidth = barWidth
+
+    this.width = canvasCtx.canvas.width
+    this.height = canvasCtx.canvas.height
+  }
+
+  drawTime (dataArray) {
+    this.canvasCtx.fillStyle = 'rgb(255, 255, 255)'
+    this.canvasCtx.fillRect(0, 0, this.width, this.height)
+
+    this.canvasCtx.lineWidth = 2
+    this.canvasCtx.strokeStyle = 'rgb(0, 100, 0)'
+
+    this.canvasCtx.beginPath()
+
+    var sliceWidth = this.width * 1.0 / this.tBufferLength
+    var x = 0
+
+    for (let i = 0; i < this.tBufferLength; i++) {
+      var v = dataArray[i] / 128
+      var y = v * this.height / 2
+
+      if (i === 0) {
+        this.canvasCtx.moveTo(x, y)
+      } else {
+        this.canvasCtx.lineTo(x, y)
+      }
+
+      x += sliceWidth
+    }
+
+    this.canvasCtx.lineTo(this.canvasCtx.width, this.canvasCtx.height / 2)
+    this.canvasCtx.stroke()
+  }
+
+  drawFrequency (bars) {
+    let x = 0
+
+    for (let i = 0; i < this.fBufferLength; i++) {
+      let barHeight = bars[i]
+
+      this.canvasCtx.fillStyle = 'rgb(' + (barHeight + 10) + ', 50, 50)'
+      this.canvasCtx.fillRect(x, this.height - 1.4 * barHeight, this.barWidth, barHeight * 1.4)
+
+      x += this.barWidth + 2
+    }
+  }
+}
+
+const defaultSkin = new AnalyserDefaultSkin()
+
 class Analyser {
-  constructor (audioContext, canvasCtx) {
+  constructor (audioContext, canvasCtx, { skin = defaultSkin } = {}) {
     this.audioContext = audioContext
     this.canvasCtx = canvasCtx
 
@@ -15,6 +71,13 @@ class Analyser {
 
     this.drawTime()
     this.drawFrequency()
+
+    this.skin = skin
+    this.skin.set({
+      fBufferLength: this.node.frequencyBinCount,
+      tBufferLength: this.node.fftSize,
+      canvasCtx
+    })
 
     this.isDrawing = false
   }
@@ -44,38 +107,12 @@ class Analyser {
     }
 
     const analyser = this.node
-    const canvasCtx = this.canvasCtx
     const bufferLength = this.t.bufferLength
     const dataArray = new Uint8Array(bufferLength)
 
     analyser.getByteTimeDomainData(dataArray)
 
-    canvasCtx.fillStyle = 'rgb(255, 255, 255)'
-    canvasCtx.fillRect(0, 0, this.width, this.height)
-
-    canvasCtx.lineWidth = 2
-    canvasCtx.strokeStyle = 'rgb(0, 100, 0)'
-
-    canvasCtx.beginPath()
-
-    var sliceWidth = this.width * 1.0 / bufferLength
-    var x = 0
-
-    for (let i = 0; i < bufferLength; i++) {
-      var v = dataArray[i] / 128
-      var y = v * this.height / 2
-
-      if (i === 0) {
-        canvasCtx.moveTo(x, y)
-      } else {
-        canvasCtx.lineTo(x, y)
-      }
-
-      x += sliceWidth
-    }
-
-    canvasCtx.lineTo(canvasCtx.width, canvasCtx.height / 2)
-    canvasCtx.stroke()
+    this.skin.drawTime(dataArray)
   }
 
   drawFrequency () {
@@ -89,17 +126,7 @@ class Analyser {
     const dataArray = new Uint8Array(this.f.bufferLength)
 
     analyser.getByteFrequencyData(dataArray)
-
-    let x = 0
-
-    for (let i = 0; i < this.f.bufferLength; i++) {
-      let barHeight = dataArray[i]
-
-      this.canvasCtx.fillStyle = 'rgb(' + (barHeight + 10) + ', 50, 50)'
-      this.canvasCtx.fillRect(x, this.height - 1.4 * barHeight, this.f.barWidth, barHeight * 1.4)
-
-      x += this.f.barWidth + 2
-    }
+    this.skin.drawFrequency(dataArray)
   }
 
   start () {

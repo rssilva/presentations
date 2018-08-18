e2da06dac90da08746cd0fa4671d0c476b3cd3aa
Rafael Specht <rafaelspdasilva@gmail.com>
2018-08-18 10:15:46 -0300

    Adding a very rudimentar way to record songs from mic and save to file

---
+++ braziljs-2018/recording/from-mic/from-mic.js
@@ -0,0 +1,80 @@
+const audioContext = new AudioContext()
+const analyser = new modules.Analyser(audioContext, document.getElementById('analyser').getContext('2d'))
+const audioUtils = new modules.AudioUtils(audioContext)
+
+const RECORDED1 = []
+
+let recorder1 = new Recorder(audioContext, { channels: 1 }) // eslint-disable-line
+let audioInput
+
+const init = (stream) => {
+  audioInput = audioContext.createMediaStreamSource(stream)
+
+  const waveshaper = new WaveShaper({ audioContext })
+  waveshaper.makeDistortionCurve(400)
+
+  recorder1.onAudioProcess((data) => {
+    RECORDED1.push(...data)
+  })
+
+  audioInput.connect(recorder1.node)
+  recorder1.node.connect(analyser.node)
+  analyser.node.connect(audioContext.destination)
+
+  analyser.start()
+
+  // stops song 1 second after starts, this will trigger 'onended' callback
+  setTimeout(() => {
+    onEnded()
+  }, 500)
+}
+
+const onEnded = () => {
+  recorder1.node.disconnect()
+  audioInput.disconnect()
+  analyser.node.disconnect()
+
+  plotGraph({
+    signals: [
+      RECORDED1.slice(0, 2000)
+    ],
+    context: document.getElementById('comparison').getContext('2d'),
+    suggestedMin: -1,
+    suggestedMax: 1,
+    colors: ['red', 'orange']
+  })
+
+  $.ajax({
+    url: 'http://localhost:3000/clear'
+  }).then(() => {
+    sendData(RECORDED1)
+  })
+}
+
+const sendData = (arr, index = 0) => {
+  const size = 500
+  const slice = arr.slice(index, index + size)
+
+  if (slice.length == 0) {
+    $.ajax({
+      url: 'http://localhost:3000/end'
+    })
+
+    return
+  }
+
+  $.ajax({
+    method: 'POST',
+    dataType: 'json',
+    url: 'http://localhost:3000/data',
+    data: { slice: slice.toString() }
+  }).then(() => {
+    index += size
+    sendData(arr, index)
+  })
+}
+
+setTimeout(() => {
+  console.log('GOOOOO')
+  audioUtils.getUserMedia(init)
+})
